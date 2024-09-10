c9d9c8a7736aac2510c4e6f4ddf43b1bd213d6c1
Thomson-dev <tomsinonyedikachi@gmail.com>
2024-09-10 07:32:42 +0100

    fixing token issue

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,18 +1,17 @@
-import User from '../models/user.model.js';
-import bcryptjs from 'bcryptjs';
-import { errorHandler } from '../utils/error.js';
-
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
 export const updateUser = async (req, res, next) => {
   // Check if the user ID from the request matches the user ID in the URL parameters
   if (req.user.id !== req.params.userId) {
-    return next(errorHandler(403, 'You are not allowed to update this user'));
+    return next(errorHandler(403, "You are not allowed to update this user"));
   }
 
   // Check if the request body contains a password
   if (req.body.password) {
     if (req.body.password.length < 6) {
-      return next(errorHandler(400, 'Password must be at least 6 characters'));
+      return next(errorHandler(400, "Password must be at least 6 characters"));
     }
     req.body.password = bcryptjs.hashSync(req.body.password, 10);
   }
@@ -20,16 +19,20 @@ export const updateUser = async (req, res, next) => {
   // Check if the request body contains a username
   if (req.body.username) {
     if (req.body.username.length < 7 || req.body.username.length > 20) {
-      return next(errorHandler(400, 'Username must be between 7 and 20 characters'));
+      return next(
+        errorHandler(400, "Username must be between 7 and 20 characters")
+      );
     }
-    if (req.body.username.includes(' ')) {
-      return next(errorHandler(400, 'Username cannot contain spaces'));
+    if (req.body.username.includes(" ")) {
+      return next(errorHandler(400, "Username cannot contain spaces"));
     }
     if (req.body.username !== req.body.username.toLowerCase()) {
-      return next(errorHandler(400, 'Username must be lowercase'));
+      return next(errorHandler(400, "Username must be lowercase"));
     }
     if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
-      return next(errorHandler(400, 'Username can only contain letters and numbers'));
+      return next(
+        errorHandler(400, "Username can only contain letters and numbers")
+      );
     }
   }
 
@@ -37,10 +40,8 @@ export const updateUser = async (req, res, next) => {
     // Find the user by ID to retain the token
     const user = await User.findById(req.params.userId);
     if (!user) {
-      return next(errorHandler(404, 'User not found'));
+      return next(errorHandler(404, "User not found"));
     }
-
- 
 
     // Update the user details
     const updatedUser = await User.findByIdAndUpdate(
@@ -56,26 +57,33 @@ export const updateUser = async (req, res, next) => {
       { new: true }
     );
 
+    // Generate a new token after updating the user
+    const token = jwt.sign(
+      {
+        id: updatedUser._id,
+        isAdmin: updatedUser.isAdmin,
+      },
+      process.env.JWT_SECRET, // Make sure to replace this with your secret key
+      { expiresIn: "1h" }
+    );
+
     // Destructure the password out of the updated user object to exclude it from the response
     const { password, ...rest } = updatedUser._doc;
 
     // Send the updated user details, including the token
-    res.status(200).json({ ...rest, token: existingToken });
+    res.status(200).json({ ...rest, token });
   } catch (error) {
     next(error);
   }
 };
 
-
 export const deleteUser = async (req, res, next) => {
-  
-  
-  if ( !req.user.isAdmin && req.user.id !== req.params.userId) {
-    return next(errorHandler(403, 'You are not allowed to delete this user'));
+  if (!req.user.isAdmin && req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to delete this user"));
   }
   try {
     await User.findByIdAndDelete(req.params.userId);
-    res.status(200).json('User has been deleted');
+    res.status(200).json("User has been deleted");
   } catch (error) {
     next(error);
   }
@@ -83,27 +91,22 @@ export const deleteUser = async (req, res, next) => {
 
 export const signout = (req, res, next) => {
   try {
-    res
-      .status(200)
-      .json('User has been signed out');
+    res.status(200).json("User has been signed out");
   } catch (error) {
     next(error);
   }
 };
 
-
 export const getUsers = async (req, res, next) => {
-  
   if (!req.user.isAdmin) {
-    return next(errorHandler(403, 'You are not allowed to see all users'));
+    return next(errorHandler(403, "You are not allowed to see all users"));
   }
   try {
     // const startIndex = parseInt(req.query.startIndex) || 0;
     // const limit = parseInt(req.query.limit) || 9;
     // const sortDirection = req.query.sort === 'asc' ? 1 : -1;
 
-    const users = await User.find()
-    
+    const users = await User.find();
 
     const usersWithoutPassword = users.map((user) => {
       const { password, ...rest } = user._doc;
