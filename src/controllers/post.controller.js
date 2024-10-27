6a08201b62f2b48a5524209f09b95b22d674a4e8
Thomson-dev <tomsinonyedikachi@gmail.com>
2024-10-27 19:21:30 +0100

    add vercel domain

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,8 +1,6 @@
 import Post from "../models/post.model.js";
 import { errorHandler } from "../utils/error.js";
 
-import { getConnectedClient } from "../config/redisClient.js";
-
 export const create = async (req, res, next) => {
   if (!req.user.isAdmin) {
     return next(errorHandler(403, "You are not allowed to create a post"));
@@ -23,38 +21,14 @@ export const create = async (req, res, next) => {
 
   try {
     const savedPost = await newPost.save();
-    // Update the cache
-    const redisClient = await getConnectedClient();
-    redisClient.set(
-      `post:${savedPost._id}`,
-      JSON.stringify(savedPost),
-      "EX",
-      3600
-    ); // Correct syn
-
     res.status(201).json(savedPost);
   } catch (error) {
     next(error);
   }
 };
 
-// Adjust the import path as needed
-
 export const getposts = async (req, res, next) => {
-  const cacheKey = "posts:all";
-
   try {
-    const redisClient = await getConnectedClient();
-
-    // Try to get data from cache
-    const cachedData = await redisClient.get(cacheKey);
-
-    if (cachedData) {
-      console.log("Data from cache");
-      return res.status(200).json(JSON.parse(cachedData));
-    }
-
-    // If not in cache, fetch from database
     const posts = await Post.find({}).sort({ createdAt: -1 });
     const totalPosts = await Post.countDocuments();
 
@@ -75,9 +49,6 @@ export const getposts = async (req, res, next) => {
       lastMonthPosts,
     };
 
-    // Cache the response
-    await redisClient.set(cacheKey, JSON.stringify(response), "EX", 3600); // Correct syntax
-
     res.status(200).json(response);
   } catch (error) {
     next(error);
@@ -86,24 +57,11 @@ export const getposts = async (req, res, next) => {
 
 export const getpost = async (req, res, next) => {
   const { id } = req.params;
-  const cacheKey = `post:${id}`;
 
   try {
-    const redisClient = await getConnectedClient();
-
-    // Try to get data from cache
-    const cachedData = await redisClient.get(cacheKey);
-
-    if (cachedData) {
-      return res.status(200).json(JSON.parse(cachedData));
-    }
-
-    // If not in cache, fetch from database
     const post = await Post.findById(id);
 
     if (post) {
-      // Cache the post data
-      await redisClient.set(cacheKey, JSON.stringify(post), "EX", 3600);
       res.status(200).json(post);
     } else {
       throw errorHandler(404, "Post not found");
@@ -132,17 +90,6 @@ export const updatepost = async (req, res, next) => {
       { new: true }
     );
 
-    if (updatedPost) {
-      // Update the cache
-      const redisClient = await getConnectedClient();
-      redisClient.set(
-        `post:${req.params.postId}`,
-        JSON.stringify(updatedPost),
-        "EX",
-        3600
-      );
-    }
-
     res.status(200).json(updatedPost);
   } catch (error) {
     next(error);
@@ -155,12 +102,6 @@ export const deletepost = async (req, res, next) => {
   }
   try {
     const deletedPost = await Post.findByIdAndDelete(req.params.postId);
-
-    if (deletedPost) {
-      // Invalidate the cache
-      const redisClient = await getConnectedClient();
-      redisClient.del(`post:${req.params.postId}`);
-    }
 
     res.status(200).json("The post has been deleted");
   } catch (error) {
